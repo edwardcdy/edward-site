64fac749758128bf4d3aeb23b572627df891fc84
Edward <edwardcdy@gmail.com>
2017-06-13 23:22:18 -0500

    Updated

--- js/script.js
+++ js/script.js
@@ -1,3 +1,52 @@
+var cb = function(index, nextIndex, direction){
+  if (nextIndex==2){
+    blog.classList.add('gray');
+    resume.classList.add('gray');
+    contact.classList.add('gray');
+  } else {
+    blog.classList.remove('gray');
+    resume.classList.remove('gray');
+    contact.classList.remove('gray');
+  } 
+}
+
 $(document).ready(function() {
-  $('#fullpage').fullpage();
+  $('#fullpage').fullpage({
+    scrollingSpeed: 600,
+    anchors: ['info','projects','reach'],
+    recordHistory: false,
+    onLeave: cb,
+    allowPageScroll: true
+  });
 });
+
+$(document).on('click', '#down', function(){
+  $.fn.fullpage.moveSectionDown();
+});
+
+$(document).on('click', '#right', function(){
+  $.fn.fullpage.moveSlideRight();
+});
+
+$(document).on('click', '#contact', function(){
+  $.fn.fullpage.moveTo('reach', 2);
+});
+
+$(document).on('click', '#facebook', function(){
+  window.location = "https://www.facebook.com/edward.cd.yao";
+});
+
+$(document).on('click', '#github', function(){
+  window.location = "https://github.com/edwardcdy";
+});
+
+$(document).on('click', '#linkedin', function(){
+  window.location = "https://www.linkedin.com/in/edward-yao-677b3465/";
+});
+
+$(document).on('click', '#send', function(){
+
+
+});
+
+
