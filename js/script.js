5298c137f91bade85d500e4ccd64bf78de269a21
Edward <edwardcdy@gmail.com>
2018-03-09 23:47:45 -0500

    Website v2 vapor~

--- js/script.js
+++ js/script.js
@@ -1,56 +1,62 @@
-var cb = function(index, nextIndex, direction){
-  if (nextIndex==2){
-    blog.classList.add('gray');
-    resume.classList.add('gray');
-    contact.classList.add('gray');
-  } else {
-    blog.classList.remove('gray');
-    resume.classList.remove('gray');
-    contact.classList.remove('gray');
-  } 
-}
+$(document).ready(function(){
 
-$(document).ready(function() {
-  $('#fullpage').fullpage({
-    scrollingSpeed: 600,
-    anchors: ['info','projects','reach'],
-    recordHistory: false,
-    onLeave: cb,
-    allowPageScroll: true,
-    loopHorizontal: false,
-    controlArrows: false,
+  $('#more').click(function(){
+    $('#landing').fadeOut({
+      duration: 1500,
+      easings: "easeOutExpo"
+    });
+
+    var newY = $('#fullpage').height()/2 - 32 - 36;
+    var newX = $('#fullpage').width()/2 - 148 - 60;
+
+    var fadeAway = function(){
+      $('#invis').fadeOut({
+        duration: 1000,
+        easings: "easeOutExpo"
+      });
+      $('#circle').fadeOut({
+        duration: 1000
+      });
+      $("#fullpage").animate({
+        backgroundColor: "#BA5C12",
+      }, 1000);
+      $('#everything').fadeIn({
+        duration: 1000
+      });
+    };
+
+    var after = function(){
+      $("#invis").animate({
+        backgroundColor: "#87e1ff",
+      }, 1200);
+      $(".name").animate({
+        color: "#ffa8d7",
+      },{
+        duration: 1200,
+        complete: fadeAway
+      });
+      $('#circle').fadeIn();
+    };
+
+    $("#invis").animate({
+        top: newY,
+        left: newX,
+        height: '64px',
+        width: '296px',
+      },{
+        duration: 1000,
+        easings: "easeInSine",
+        complete: after
+    });
+
+    $(".name").animate({
+        color: "#fffb96"
+      },{
+        duration: 1000,
+        easings: "easeInSine",
+    });
     
+
   });
+
 });
-
-$(document).on('click', '#down', function(){
-  $.fn.fullpage.moveSectionDown();
-});
-
-$(document).on('click', '#right', function(){
-  $.fn.fullpage.moveSlideRight();
-});
-
-$(document).on('click', '#left', function(){
-  $.fn.fullpage.moveSlideLeft();
-});
-
-$(document).on('click', '#contact', function(){
-  $.fn.fullpage.moveTo('reach', 2);
-});
-
-$(document).on('click', '#facebook', function(){
-  window.location = "https://www.facebook.com/edward.cd.yao";
-});
-
-$(document).on('click', '#github', function(){
-  window.location = "https://github.com/edwardcdy";
-});
-
-$(document).on('click', '#linkedin', function(){
-  window.location = "https://www.linkedin.com/in/edward-yao-677b3465/";
-});
-
-
-
-
